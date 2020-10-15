32d9b6fa81e044d527ee0c6087bfb8768d178f16
cesarsld <jaimes.cesar88@gmail.com>
2020-10-16 00:37:18 +0200

    catch

--- SquigglyBuyBot/Loop.cs
+++ SquigglyBuyBot/Loop.cs
@@ -11,25 +11,33 @@ namespace SquigglyBuyBot
             int safetyCheck = 0;
             while (true)
             {
-                int unixTime = Convert.ToInt32(((DateTimeOffset)(DateTime.UtcNow)).ToUnixTimeSeconds());
-                Console.Clear();
-                int lastAuctionTime = await Web3Handler.GeteTimeOfLastStart();
-                Console.WriteLine($"Time before next update :  {(lastAuctionTime - unixTime)} seconds");
-                if (unixTime - lastAuctionTime > 0)
+                try
                 {
-                    try
+                    int unixTime = Convert.ToInt32(((DateTimeOffset)(DateTime.UtcNow)).ToUnixTimeSeconds());
+                    Console.Clear();
+                    int lastAuctionTime = await Web3Handler.GeteTimeOfLastStart();
+                    Console.WriteLine($"Time before next update :  {(lastAuctionTime - unixTime)} seconds");
+                    if (unixTime - lastAuctionTime > 0)
                     {
-                        await Web3Handler.GetInAuctionSquiggly();
-                        await Task.Delay(60000 * 15);
+                        try
+                        {
+                            await Web3Handler.GetInAuctionSquiggly();
+                            await Task.Delay(60000 * 15);
+                        }
+                        catch (Exception e)
+                        {
+                            safetyCheck++;
+                            Console.WriteLine(e.Message);
+                            if (safetyCheck == 5)
+                                return;
+                        }
                     }
-                    catch (Exception e) {
-                        safetyCheck++;
-                        Console.WriteLine(e.Message);
-                        if (safetyCheck == 5)
-                            return;
-                    }
+                    await Task.Delay(60000 * 5);
                 }
-                await Task.Delay(60000 * 5);
+                catch (Exception e) {
+                    Console.WriteLine(e.Message);
+                    await Task.Delay(30000);
+                }
             }
         }
     }
